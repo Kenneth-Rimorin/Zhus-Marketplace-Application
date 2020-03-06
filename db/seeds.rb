aceade19787a7947b487c7ed0c0a1a41dcedf826
Kenneth <rimorinkennet@gmail.com>
2020-03-06 13:06:15 +1100

    seeded users & created listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,4 +22,12 @@ brands.each do |brand|
 
 end
 
-Listing.create(name:"Nike React Element", color:"Red/black",size:12, price:100, description:"Not used, in very good condition", brand:Brand.find(1), category:"casual")
+User.create(email:"user1@yehey.com", password: "111111", name:"Kenneth")
+User.create(email:"user2@yehey.com", password: "111111", name:"Longy")
+User.create(email:"user3@yehey.com", password: "111111", name:"Master")
+User.create(email:"user4@yehey.com", password: "111111", name:"Richard")
+User.create(email:"user5@yehey.com", password: "111111", name:"Warrior")
+User.create(email:"user6@yehey.com", password: "111111", name:"Boy")
+
+puts "Users Created"
+
